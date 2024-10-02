c4b19d0eca6e705a736c3f91636e20fd5aebf27e
Wariha khan <150653055+warihakhan2@users.noreply.github.com>
2024-10-02 13:15:16 +0500

    Add files via upload

---
+++ 6th online class/main.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+let todos = [];
+let condition = true;
+while(condition)
+{let todosQuestion = await inquirer.prompt([
+
+
+        {
+            name: "firstQuestion",
+            message: chalk.yellow("What would you like to add in your Todos?"),
+            type:"input",
+        } ,  
+        {
+            name: "secondQuestion",
+            message: chalk.green("Would you like to add more in your Todos?"),
+            type: "confirm",
+            default: "true",
+        }
+    ]
+);
+todos.push(todosQuestion.firstQuestion);
+console.log(chalk.blue(todos));
+condition = todosQuestion.secondQuestion;
+}
